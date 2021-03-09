a94e31c78514a5cbe60a70c15eceac44fb5bfc2f
seufernandez <fernandez.g1337@gmail.com>
2021-03-09 09:24:32 -0800

    Class1 Finished

---
+++ js/principal.js
@@ -0,0 +1,9 @@
+//mudar o h1 pelo Js
+
+//pegar conteudo do .titulo 
+var titulo = document.querySelector(".titulo");
+		console.log(titulo.textContent);
+
+		// mudar o conteudo do h1/.titulo(se alguem quiser mudar o h1 p h2 vai poder por agora a variavel busca pela classe)
+		titulo.textContent = "Aparecida Nutricionista";
+		
